48562d53e95d526e674e465603d4c697faa3425a
eved4 <ucabeda@ucl.ac.uk>
2021-03-12 21:00:37 +0000

    updates

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //imports
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 //static files
 app.use(express.static('public'))
